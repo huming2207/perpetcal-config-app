4aaeed5121cb76892b158503e0ebcdda02f1fafc
Jackson Ming Hu <huming2207@gmail.com>
2022-05-30 22:54:19 +1000

    config_kv_pkt: init

---
+++ lib/protocol/config_kv_packet.dart
@@ -0,0 +1,18 @@
+import 'package:perpetcal/protocol/packet_header.dart';
+import 'package:perpetcal/protocol/pkt_types.dart';
+
+class SetUint32Packet {}
+
+class GetUint32Packet {}
+
+class SetInt32Packet {}
+
+class GetInt32Packet {}
+
+class SetBlobPacket {}
+
+class GetBlobPacket {}
+
+class SetStringPacket {}
+
+class GetStringPacket {}
