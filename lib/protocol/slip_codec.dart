ec2985e182ff9997fe93a297cf7d4761d5845d3b
Jackson Ming Hu <huming2207@gmail.com>
2022-05-30 21:34:13 +1000

    SlipCodec: initial impl for stream-based SLIP codec

--- lib/protocol/slip_codec.dart
+++ lib/protocol/slip_codec.dart
@@ -1,18 +1,91 @@
-import 'dart:ffi';
 import 'dart:typed_data';
 
 const slipEnd = 0xc0;
 const slipEsc = 0xdb;
 const slipEscEnd = 0xdc;
 const slipEscEsc = 0xdd;
-const maxPacketLen = 520; // Approx worst case scenario packet length for raw packet (after SLIP encoded + with header)
-
 const slipEscEndSeq = [slipEsc, slipEscEnd];
 const slipEscEscSeq = [slipEsc, slipEscEsc];
 
 class SlipCodec {
-  // Future<Uint8List> decodeFromStream(Stream<Uint8List> stream) async {
-  //   await for (final bytes in stream) {}
+  var recvStarted = false;
+  var escDetected = false;
+  final decodedBuilder = BytesBuilder();
+  Stream<Uint8List> recvStream;
+  Function(Uint8List) onPacketRecv;
 
-  // }
+  SlipCodec(this.recvStream, this.onPacketRecv) {
+    recvStream.listen(_onRecvData, onError: null, onDone: _onRecvDone);
+  }
+
+  Uint8List encode(Uint8List data) {
+    final encodedData = BytesBuilder();
+    encodedData.addByte(slipEnd);
+    for (final byte in data) {
+      if (byte == slipEnd) {
+        encodedData.add(slipEscEndSeq);
+      } else if (byte == slipEsc) {
+        encodedData.add(slipEscEscSeq);
+      } else {
+        encodedData.addByte(byte);
+      }
+    }
+    encodedData.addByte(slipEnd);
+    return encodedData.toBytes();
+  }
+
+  void _onRecvData(Uint8List bytes) {
+    for (final byte in bytes) {
+      switch (byte) {
+        case slipEnd:
+          {
+            if (recvStarted) {
+              recvStarted = false;
+              escDetected = false;
+              onPacketRecv(decodedBuilder.toBytes());
+              break;
+            } else {
+              recvStarted = true;
+              escDetected = false;
+              decodedBuilder.clear();
+            }
+            break;
+          }
+
+        case slipEsc:
+          {
+            escDetected = true;
+            break;
+          }
+
+        case slipEscEnd:
+          {
+            if (escDetected) {
+              decodedBuilder.addByte(slipEnd);
+            } else {
+              decodedBuilder.addByte(slipEscEnd);
+            }
+            break;
+          }
+
+        case slipEscEsc:
+          {
+            if (escDetected) {
+              decodedBuilder.addByte(slipEsc);
+            } else {
+              decodedBuilder.addByte(slipEscEsc);
+            }
+            break;
+          }
+
+        default:
+          {
+            decodedBuilder.addByte(byte);
+            break;
+          }
+      }
+    }
+  }
+
+  void _onRecvDone() {}
 }
