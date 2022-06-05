b102cea4a9bf08f57da3eaa552048fa10633b011
Jackson Ming Hu <huming2207@gmail.com>
2022-06-05 23:00:18 +1000

    ChunkAckPacket: init

---
+++ lib/protocol/model/chunk_ack_pkt.dart
@@ -0,0 +1,13 @@
+import 'dart:typed_data';
+
+import 'package:perpetcal/protocol/pkt_types.dart';
+
+class ChunkAckPacket {
+  late ChunkAckState ackState;
+  late int auxInfo;
+
+  ChunkAckPacket(Uint8List bytes) {
+    ackState = UsbPacketTypeHelper.byteToChunkAck(bytes[0]);
+    auxInfo = ByteData.sublistView(bytes.sublist(1, 5)).getUint32(0);
+  }
+}
